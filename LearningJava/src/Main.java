5a716cf9b0385fdb4d5f44580f707549d3adcaa2
hstegemo <hstegemo@gmail.com>
2022-07-02 18:27:42 +0200

    New project for Learning Java

---
+++ LearningJava/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        String helge = "Helge";
+        char firstInitial = helge.charAt(0);
+        System.out.println(firstInitial);
+    }
+}
